ef38fb725ffb697d437fc3c72f8b8f1039a5b1c4
aarav1206 <roydustin60@gmail.com>
2024-09-25 22:06:10 +0530

    Add files via upload

---
+++ Output.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+
+    int age = 22;
+    int pi = 3.14;
+    char percentage = '%';
+
+    printf("age is %d", age);
+    printf("age is %f", pi);
+    printf("age is %c", percentage);
+    return 0;
+}
