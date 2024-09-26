53826d1dfc65061ffc90fc985e15f86faae0415d
aarav1206 <roydustin60@gmail.com>
2024-09-26 08:16:50 +0530

    Add files via upload

---
+++ Chapter-1/03_Problem.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int r = 3;
+    int height = 10;
+    printf("The is area of crircle with radius %d is %f\n" , r , 3.14*r*r);
+    printf("The is Volume of cylinder with radius %d and height %d is %f" , r , height , 3.14*r*r*height);
+    return 0;
+}
