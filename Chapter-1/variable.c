f6eb22c2f68a30e7d326966377636647d9cf7646
aarav1206 <roydustin60@gmail.com>
2024-09-27 14:48:49 +0530

    Add files via upload

---
+++ Chapter-1/variable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int a , b , c;
+    a = 10;
+    b = 12;
+    c = 23;
+    float d = 13.49068;
+    // string name = "Aarav";
+    printf("Value of a %d\n" , a);
+    printf("Value of b %d\n" , b);
+    printf("Value of c %d\n" , c);
+    printf("Value of d %f\n" , d);
+    // printf("%s\n");
+    return 0;
+}
