6bebd7e185d58a9edff70c84642abfb956746cc7
aarav1206 <roydustin60@gmail.com>
2024-09-28 11:13:03 +0530

    Update sum.c
    updating float value

--- Chapter-1/sum.c
+++ Chapter-1/sum.c
@@ -2,6 +2,7 @@
 
 int main(){
     int a, b;
+    float c;
     printf("enter a:");
     scanf("%d" , &a);
 
@@ -10,10 +11,11 @@ int main(){
 
     // int sum = a + b;
     // printf("Sum is:%d" , sum);
+    c = a/b;
     printf("Sum is:%d\n" , a + b); //we can use directly operation without creating variables
     printf("Subtraction is:%d\n" , a - b);
     printf("Multiplication is:%d\n" , a * b);
-    printf("Division is:%f\n" , a / b);
+    printf("Division is:%f\n" , c);
     printf("Modulus is:%d\n" , a % b);    //Modulus gives remainder and it is use for to find prime number and so on.
     return 0;
     
