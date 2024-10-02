0d9f27a1b887ad562db364981173bfab50775eb5
aarav1206 <roydustin60@gmail.com>
2024-10-02 23:21:16 +0530

    Create if_else.c

---
+++ Chapter-3/if_else.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int age = 15;
+
+    if (age>10)
+    {
+        printf("your age is greater than 10");
+    }
+
+    else
+    {
+        printf("your age is not greater than 10");
+    }
+    
+    return 0;
+}
