4e2ecbd62f69dc364d38abd61f1f034a8be06cf1
aarav1206 <roydustin60@gmail.com>
2024-10-02 23:41:02 +0530

    Add files via upload

---
+++ Chapter-3/ternary.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    // condition ? expression-if-true : expression-if-false
+    int a = 5;
+    int b = 7;
+
+    a>b?printf("a is greater than b"):printf("b is greater than a");
+    
+    return 0;
+}
